a2fc3749354efc0083d6fd2a5a26434e92a28250
verde <andres.laverde@protonmail.com>
2024-09-21 20:02:48 -0700

    finish ex 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,17 +3,21 @@ import { useState } from 'react'
 const App = () => {
   //state variables
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone: '040-1234567' }
   ]); 
   const [newName, setNewName] = useState('');
+  const [newPhone, setNewPhone] = useState('');
 
   //Returns true if any entry is duplicate, case insensitive
-  const checkDuplicate = (newEntry) => persons.some((person) => JSON.stringify(person.name.toLowerCase()) === JSON.stringify(newEntry.toLowerCase()))
+  const checkDuplicate = (newEntry) => { 
+    return persons.some((person) => JSON.stringify(person.name.toLowerCase()) === JSON.stringify(newEntry.toLowerCase()) || JSON.stringify(person.phone) === JSON.stringify(newEntry)) 
+
+  }
 
   //Submit form handler
   const addPerson = (event) =>{
     event.preventDefault();
-    if(checkDuplicate(newName)) {
+    if(checkDuplicate(newName)|| checkDuplicate(newPhone)) {
       alert(`${newName} is already added to phonebook`);
       return;
     }
@@ -26,6 +30,10 @@ const App = () => {
     console.log(event.target.value);
     setNewName(event.target.value);
   }
+  const handlePhoneChange = (event) =>{
+    console.log(event.target.value);
+    setNewPhone(event.target.value);
+  }
   return (
     <div>
       <h2>Phonebook</h2>
@@ -34,11 +42,14 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          phone: <input value={newPhone} onChange={handlePhoneChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map(person => <p key={person.name}>{person.name}: {person.phone}</p>)}
     </div>
   )
 }
