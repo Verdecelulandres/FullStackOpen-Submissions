982e218cf60441de4bdd9973ac0d6b89ceb67bd9
verde <andres.laverde@protonmail.com>
2024-09-30 17:30:32 -0700

    Made services with axios

---
+++ part2/countryInfo/src/services/country.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/';
+
+const getAllCountries = () => axios.get(baseUrl+"all");
+const getSelectedCountry =(countryName)=> axios.get(`${baseUrl}name/${countryName}`)
+export default {getAllCountries, getSelectedCountry}
