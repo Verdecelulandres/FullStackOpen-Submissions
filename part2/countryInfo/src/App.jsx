82ac572c95e1408abb3e698045d82e9824500581
verde <andres.laverde@protonmail.com>
2024-09-30 18:37:54 -0700

    implement search

--- part2/countryInfo/src/App.jsx
+++ part2/countryInfo/src/App.jsx
@@ -6,23 +6,48 @@ import country from "./services/country";
 const App = () => {
   //Array to hold the API response
   const [countries, setCountries] = useState([]);
-  //Value to keep track of the country to search for in the input element
-  const [countrySearch, setCountrySearch] = useState('');
+
   const [filteredCountries, setFilteredCountries] = useState ([]); 
 
   useEffect(() => {
     country.getAllCountries().then(response => {
       console.log('Got All countries');
-      setCountries(response.data)
+      setCountries(response.data);
+      // setTimeout(()=>console.log(`Number of countries: ${countries.length}`), 2000);
     })
     .catch(error => {
       console.log(error);
       alert('There was a problem, please refresh the page');
     });
   }, []);
+
+  //Use only for debugging: See which countries are getting filtered
+  useEffect(()=>{
+    console.log('inside filtered countries useeffect');
+    filteredCountries.map( c => console.log(c.name.common));
+  }, [filteredCountries]);
+
+  const filterCountries = (event) =>{
+    if(countries.lenght === 0) {
+      return;
+    }
+    const searchInput = event.target.value.toLowerCase();  
+    if(searchInput === '') {
+      console.log('Search input is empty, resetting the filtered array');
+      setFilteredCountries([]);
+    } else {
+      console.log(`search input: ${searchInput}`);
+    setFilteredCountries(countries.filter(c => c.name.common.toLowerCase().includes(searchInput)));  
+    }
+    
+  }
+
   return(
     <div>
-      <p>Find Countries: <input /></p>
+     Find Countries: <input onChange={filterCountries}/>
+     <ul>
+      {filteredCountries.length >10 ? <li>Too many matches. Please continue typing the country name</li> : filteredCountries.map(c=> <li key={c.cca2}>{c.name.common}</li>)}
+     </ul>
     </div>
   )
 }
